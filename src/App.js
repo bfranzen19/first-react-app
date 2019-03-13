e1a522bf18b85f15e210387ae722abea3eaf3f8a
bt <btfranzen@gmail.com>
2019-03-12 20:52:45 -0600

    playing with nesting elements in a component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,21 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
+    // return (   // use this way of nesting elements, not the below way.
+    //   <div className="App">
+    //     <h1> i'll show you! </h1>
+    //   </div>
+    // );
+
+    return (   // use this way of nesting elements, not the below way.
       <div className="App">
-        <h1>i'm a react app</h1>
+        <h1> i'll show you! </h1>
+        <p> figuring this out </p>
       </div>
     );
+
+    // React.createElement() takes at least 3 args. elem we want to render to the DOM (could be an imported component), configuration for this (JS object, can be null), any amount of children (can have multiple args separated by commas)
+    // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, `i'm a react app!`)); // must nest React.createElement() calls to nest elements like this. we don't really use this way because it can get really complicated.
   }
 }
 
