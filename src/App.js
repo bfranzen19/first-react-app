668326c5e542d6ed1194109051b44c76ecc8e245
bt franzen <bt.franzen@zayo.com>
2019-03-13 19:14:56 -0600

    working with props, state, and event handlers

--- src/App.js
+++ src/App.js
@@ -2,25 +2,116 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
+// class App extends Component {
+//   render() {
+//     // return (   // use this way of nesting elements, not the below way.
+//     //   <div className="App">
+//     //     <h1> i'll show you! </h1>
+//     //   </div>
+//     // );
+//
+//     // // using a component
+//     // return (   // use this way of nesting elements, not the below way.
+//     //   <div className="App">
+//     //     <h1> i'll show you! </h1>
+//     //     <p> figuring this out </p>
+//     //     <Person/>
+//     //     <Person/>
+//     //   </div>
+//     );
+//
+//     // React.createElement() takes at least 3 args. elem we want to render to the DOM (could be an imported component), configuration for this (JS object, can be null), any amount of children (can have multiple args separated by commas)
+//     // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, `i'm a react app!`)); // must nest React.createElement() calls to nest elements like this. we don't really use this way because it can get really complicated.
+//   }
+// }
+// export default App;
+
+
+// // using props with a component
+// class App extends Component {
+//   render() {
+//     return (   // use this way of nesting elements, not the below way.
+//         <div className="App">
+//             <Person name="bt" age="32"/>
+//         </div>
+//     );
+//   }
+// }
+// export default App;
+
+
+// accessing the info inside the component tags
+// class App extends Component {
+//     state = {
+//         persons: [
+//             { name: 'bt', age: 32 },
+//             { name: 'alby', age: 2 },
+//             { name: 'homer', age: 9 }
+//         ]
+//     }
+//
+//     render() {
+//       return (   // use this way of nesting elements, not the below way.
+//           <div className="App">
+//             <button> switch name </button>
+//             <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+//             <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+//             <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+//           </div>
+//       );
+//     }
+// }
+// export default App;
+
+
+// props and state & event handling
 class App extends Component {
-  render() {
-    // return (   // use this way of nesting elements, not the below way.
-    //   <div className="App">
-    //     <h1> i'll show you! </h1>
-    //   </div>
-    // );
+    state = {
+        persons: [
+            { name: 'bt', age: 32 },
+            { name: 'alby', age: 2 },
+            { name: 'homer', age: 9 },
+            { name: 'best buddy', age: 23 }
+        ]
+    }
 
-    return (   // use this way of nesting elements, not the below way.
-      <div className="App">
-        <h1> i'll show you! </h1>
-        <p> figuring this out </p>
-        <Person/>
-      </div>
-    );
+    switchNameHandler = () => {
+        console.log('switched ittttt');
+        // this.state.persons[0].name = 'butthole'; // don't do this!
+        // setState() takes an object as an argument. it will merge whatever we define here with our existing state.
+        this.setState({
+            persons: [
+                { name: 'bt', age: 32 },
+                { name: 'albus humblewhore', age: 2 },
+                { name: 'homer bacon', age: 100 },
+                { name: 'amy', age: 123 }
+            ]
+        });
+    }
 
-    // React.createElement() takes at least 3 args. elem we want to render to the DOM (could be an imported component), configuration for this (JS object, can be null), any amount of children (can have multiple args separated by commas)
-    // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, `i'm a react app!`)); // must nest React.createElement() calls to nest elements like this. we don't really use this way because it can get really complicated.
-  }
+    switchNameBackHandler = () => {
+        console.log('switched bacccck');
+        this.setState({
+            persons: [
+                { name: 'bt', age: 32 },
+                { name: 'alby', age: 2 },
+                { name: 'homer', age: 9 },
+                { name: 'best buddy', age: 23 }
+            ]
+        });
+    }
+
+    render() {
+      return (   // button onClick={this.functionName}
+          <div className="App">
+            <button onClick={this.switchNameHandler}> switch name </button>
+            <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+            <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+            <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+            <Person name={this.state.persons[3].name} age={this.state.persons[3].age}/>
+            <button onClick={this.switchNameBackHandler}> switch back </button>
+          </div>
+      );
+    }
 }
-
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,58 @@
+// import React from 'react';
+//
+// // dynamic stuff in a component
+// const person = () => {
+//     return <p>  i'm a person and i am { Math.floor(Math.random() * 30) } years old </p>
+// };
+//
+// export default person;
+
+// working with properties
+// import React from 'react';
+//
+// const person = (props) => {
+//     return <p>  i'm {props.name} and i am {props.age} years old </p>
+// };
+//
+// export default person;
+
+
+
+// accessing the info inside the component tags
+// import React from 'react';
+//
+// const person = (props) => {
+//     return (
+//         <div>
+//             <p>  i'm {props.name} and i am {props.age} years old </p>
+//             <p>{props.children}</p>
+//         </div>
+//     )
+// };
+// export default person;
+
+
+// understanding and using state
+// import React from 'react';
+//
+// const person = (props) => {
+//     return (
+//         <div>
+//             <p> i'm {props.name} and i am {props.age} years old </p>
+//         </div>
+//     )
+// };
+// export default person;
+
+
+// props and state
 import React from 'react';
 
-const person = () => {
-    return 8 * 3;
+const person = (props) => {
+    return (
+        <div>
+            <p> i'm {props.name} and i am {props.age} years old </p>
+        </div>
+    )
 };
-
 export default person;
