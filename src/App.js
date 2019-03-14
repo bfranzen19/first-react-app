6b490b2120b3b1637c2c23f003e22cd716ae6deb
bt franzen <bt.franzen@zayo.com>
2019-03-14 11:44:16 -0600

    state, hooks, and stateful vs stateless components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person';
+// import React, { Component } from 'react';
+// import './App.css';
+// import Person from './Person/Person';
 
 // class App extends Component {
 //   render() {
@@ -28,6 +28,10 @@ import Person from './Person/Person';
 
 
 // // using props with a component
+// import React, { Component } from 'react';
+// import './App.css';
+// import Person from './Person/Person';
+
 // class App extends Component {
 //   render() {
 //     return (   // use this way of nesting elements, not the below way.
@@ -41,6 +45,9 @@ import Person from './Person/Person';
 
 
 // accessing the info inside the component tags
+// import React, { Component } from 'react';
+// import './App.css';
+// import Person from './Person/Person';
 // class App extends Component {
 //     state = {
 //         persons: [
@@ -65,53 +72,151 @@ import Person from './Person/Person';
 
 
 // props and state & event handling
-class App extends Component {
-    state = {
+// import React, { Component } from 'react';
+// import './App.css';
+// import Person from './Person/Person';
+// class App extends Component {
+//     state = {
+//         persons: [
+//             { name: 'bt', age: 32 },
+//             { name: 'alby', age: 2 },
+//             { name: 'homer', age: 9 },
+//             { name: 'best buddy', age: 23 }
+//         ]
+//     }
+//
+//     switchNameHandler = () => {
+//         console.log('switched ittttt');
+//         // this.state.persons[0].name = 'butthole'; // don't do this!
+//         // setState() takes an object as an argument. it will merge whatever we define here with our existing state.
+//         this.setState({
+//             persons: [
+//                 { name: 'bt', age: 32 },
+//                 { name: 'albus humblewhore', age: 2 },
+//                 { name: 'homer bacon', age: 100 },
+//                 { name: 'amy', age: 123 }
+//             ]
+//         });
+//     }
+//
+//     switchNameBackHandler = () => {
+//         console.log('switched bacccck');
+//         this.setState({
+//             persons: [
+//                 { name: 'bt', age: 32 },
+//                 { name: 'alby', age: 2 },
+//                 { name: 'homer', age: 9 },
+//                 { name: 'best buddy', age: 23 }
+//             ]
+//         });
+//     }
+//
+//     render() {
+//       return (   // button onClick={this.functionName}
+//           <div className="App">
+//             <button onClick={this.switchNameHandler}> switch name </button>
+//             <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+//             <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+//             <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+//             <Person name={this.state.persons[3].name} age={this.state.persons[3].age}/>
+//             <button onClick={this.switchNameBackHandler}> switch back </button>
+//           </div>
+//       );
+//     }
+// }
+// export default App;
+
+
+
+// using the useState() hook for state manipulation (react hooks), react 16.8 >
+// import React, { useState } from 'react';    // most important react hook useState allows us to use state in a functional component instead of a class component.
+// import './App.css';
+// import Person from './Person/Person';
+//
+// // functional component declaration, not class
+// // pass initial state. use destructuring to pull elements out of the array from the right side of the equal sign (useState() function call). personsState accesses all of the persons object data. therefore, must replace this.state with personsState since this.state doesn't exist anymore since it's a functional component. setPersonsState allows us to set a new state. set the switchNameHandler to a constant and it will work fine. remove this before switchNameHandler though since it's no longer within a class.
+// const app = props => {
+//     const [personsState, setPersonsState] = useState({
+//         persons: [
+//             { name: 'alby', age: 2 },
+//             { name: 'homer', age: 9 },
+//             { name: 'best buddy', age: 23 }
+//         ]
+//     });
+//
+//     const [otherState, setOtherState] = useState({
+//         otherState: 'some other value'
+//     });
+//
+//     // never called setOtherState but it would be the same idea as switchNameHandler()
+//
+//     const switchNameHandler = () => {   // call without parenthesis and without this
+//         console.log('switched ittttt');
+//         setPersonsState({   // make this the above function call, not this.setState
+//             persons: [
+//                 { name: 'albus humblewhore', age: 2 },
+//                 { name: 'homer bacon', age: 100 },
+//                 { name: 'amy', age: 123 }
+//             ],
+//             otherState: personsState.otherState  // this will copy in the old, untouched otherState with the new object. this is one way to do it, you can also use useState() multiple times.
+//         });
+//     }
+//
+//     return (   // button onClick={this.functionName}
+//         <div className="App">
+//             <button onClick={switchNameHandler}> switch name </button>
+//             <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+//             <Person name={personsState.persons[1].name} age={personsState.persons[1].age}/>
+//             <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
+//             </div>
+//     );
+// }
+// export default app;
+//
+// // when using react hooks, the function (2nd part of the array aka setPersonsState), does NOT merge whatever you pass to it with the old state, instead it REPLACES the old state with it. this means that whenever you're updating using hooks, you have to include ALL OLD STATE DATA. to accomplish this in this example, you have to add otherState: personsState.otherState to the setPersonsState object in the switchNameHandler() function. or, instead, you can call useState() again and pass in {otherState: 'some other value'} or just the otherState value 'some other values'
+
+
+
+// stateless vs stateful components
+// stateful = a component that manages state (either through a class-based this.setState() or through a hooks/functional approach using setState()). use stateful as little as often. makes the app easier to maintain. 
+// stateless = no internal state management within the component. good practice to create as much stateless components as possible in the application
+
+import React, { useState } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+const app = props => {
+    const [personsState, setPersonsState] = useState({
         persons: [
-            { name: 'bt', age: 32 },
             { name: 'alby', age: 2 },
             { name: 'homer', age: 9 },
             { name: 'best buddy', age: 23 }
         ]
-    }
+    });
 
-    switchNameHandler = () => {
+    const [otherState, setOtherState] = useState({
+        otherState: 'some other value'
+    });
+
+    const switchNameHandler = () => {
         console.log('switched ittttt');
-        // this.state.persons[0].name = 'butthole'; // don't do this!
-        // setState() takes an object as an argument. it will merge whatever we define here with our existing state.
-        this.setState({
+        setPersonsState({
             persons: [
-                { name: 'bt', age: 32 },
                 { name: 'albus humblewhore', age: 2 },
                 { name: 'homer bacon', age: 100 },
                 { name: 'amy', age: 123 }
-            ]
+            ],
+            otherState: personsState.otherState
         });
     }
 
-    switchNameBackHandler = () => {
-        console.log('switched bacccck');
-        this.setState({
-            persons: [
-                { name: 'bt', age: 32 },
-                { name: 'alby', age: 2 },
-                { name: 'homer', age: 9 },
-                { name: 'best buddy', age: 23 }
-            ]
-        });
-    }
-
-    render() {
-      return (   // button onClick={this.functionName}
-          <div className="App">
-            <button onClick={this.switchNameHandler}> switch name </button>
-            <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-            <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-            <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-            <Person name={this.state.persons[3].name} age={this.state.persons[3].age}/>
-            <button onClick={this.switchNameBackHandler}> switch back </button>
-          </div>
-      );
-    }
+    return (
+        <div className="App">
+            <button onClick={switchNameHandler}> switch name </button>
+            <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+            <Person name={personsState.persons[1].name} age={personsState.persons[1].age}/>
+            <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
+            </div>
+    );
 }
-export default App;
+export default app;
